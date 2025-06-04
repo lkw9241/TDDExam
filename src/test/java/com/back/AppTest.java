cebe7319b08648829681682ed47a0a4597188698
LeeKW <lkw9241@gmail.com>
2025-06-04 17:51:05 +0900

    step 1, r

--- src/test/java/com/back/AppTest.java
+++ src/test/java/com/back/AppTest.java
@@ -3,11 +3,17 @@ package com.back;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
     @Test
     @DisplayName("테스트 1")
     void t1(){
+        App app= new App();
+        int rs = app.plus(1,2);
 
+
+        assertEquals(3, rs);//검증식 -> rs값이 3인것을 보장합니다.
 
     }
 }
