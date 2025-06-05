233eab5f2f9d693ca379f91fad31cc6c2cba4c1c
LeeKW <lkw9241@gmail.com>
2025-06-05 09:24:02 +0900

    step 4, b

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -7,9 +7,12 @@ public class App {
     }
 
     public int plus(int num1, int num2) {
-        if (num1 == 10 && num2 == 20) return 30;
-        if (num1 == 0 && num2 == 5) return 5;
-
-        return 3;
+        return num1 + num2;
     }
-}
+
+//        if (num1 == 10 && num2 == 20) return 30;
+//        if (num1 == 0 && num2 == 5) return 5;
+//        //TDD 장점 : (1)패턴을 파악할 수 있다. (2)내가 틀렸는지 바로 알 수 있다.
+//        return 3;
+    }
+
